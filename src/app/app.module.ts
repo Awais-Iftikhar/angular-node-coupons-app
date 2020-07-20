e8cea2c6c6bd31d2a23e53c25cad5daf20b3c431
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-20 17:46:55 +0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularmaterialModule } from './angularmaterial/angularmaterial/angularmaterial.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularmaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
