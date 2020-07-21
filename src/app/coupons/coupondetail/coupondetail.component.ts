d0d88f91110e6206a7eb485b660cbbd5b277e7ee
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-21 17:56:07 +0500

    discount conflict resolved

--- src/app/coupons/coupondetail/coupondetail.component.ts
+++ src/app/coupons/coupondetail/coupondetail.component.ts
@@ -12,12 +12,25 @@ export class CoupondetailComponent implements OnInit {
 
   couponid;
   coupondata;
+  discountvalue;
+  currency;
   constructor(private couponservice: CouponService , private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe((res) => {
       this.couponid = res.get('couponid');
       this.coupondata = this.couponservice.getsinglecoupon(this.couponid);
+      const percentdiscount = this.coupondata.percent_off;
+      const amountdiscount = this.coupondata.amount_off;
+      const currencyvalue = this.coupondata.currency;
+
+      if (amountdiscount === null || currencyvalue === null) {
+        this.discountvalue = percentdiscount;
+        this.currency = ' %';
+      } else {
+        this.discountvalue = amountdiscount;
+        this.currency = currencyvalue;
+      }
     });
   }
 
