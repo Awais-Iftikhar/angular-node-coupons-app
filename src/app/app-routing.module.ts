f5134b94ad288e9eaa5442d1d72727772f9cb45c
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-21 17:18:01 +0500

    Create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoupondetailComponent } from './coupons/coupondetail/coupondetail.component';
+import { ListofcouponsComponent } from './coupons/listofcoupons/listofcoupons.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: ListofcouponsComponent},
+  {path: 'coupon/:couponid' , component: CoupondetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
